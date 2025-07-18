169a59e238f8798f107b289d6492463d0ebf2ec5
noobdevsucksatprogram <giocarone9@gmail.com>
2025-07-18 13:55:14 -0400

    Add files via upload

---
+++ eye.py
@@ -0,0 +1,19 @@
+from typing import Self
+import pyautogui
+def get(a):
+    import pyautogui
+    print(object,a)
+    pyautogui.getInfo
+    import cv2
+    cv2.imread('wawawa.jpg')
+    pyautogui.size
+    pyautogui.position
+    pyautogui.screenshot
+get(object)
+def move():
+    get(object)
+    pyautogui.move('wawawa.jpg')
+    return object
+move()
+def exist():
+  return Self
